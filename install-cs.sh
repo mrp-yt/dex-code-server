b15b208551fd375bd2610ba34da1f25cc6907740
mrp-yt <88549505+mrp-yt@users.noreply.github.com>
2021-08-31 12:33:32 +0100

    Update install-cs.sh

--- install-cs.sh
+++ install-cs.sh
@@ -4,5 +4,4 @@ pkg install yarn -y &&
 pkg install build-essential -y &&
 pkg install python -y &&
 pkg install git -y &&
-yarn global add code-server &&
-rm code-server-install.sh
+yarn global add code-server
