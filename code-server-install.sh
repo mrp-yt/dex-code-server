da16746d4da733f1f5c97c76730d96ae4747752f
mrp-yt <88549505+mrp-yt@users.noreply.github.com>
2021-08-31 11:29:02 +0100

    Update code-server-install.sh

--- code-server-install.sh
+++ code-server-install.sh
@@ -4,5 +4,5 @@ pkg install yarn -y &&
 pkg install build-essential -y &&
 pkg install python -y &&
 pkg install git -y &&
-yarn global add code-server
-
+yarn global add code-server &&
+rm code-server-install.sh
